e8ee84db6e7c68fdc8fcb44e0c15637882fd5e12
Houle Adrien <adrienhoule01@gmail.com>
2023-10-03 12:52:55 +0200

    full trajectory with contact point

--- SMAviong/Assets/Scripts/spawnTrajectory.cs
+++ SMAviong/Assets/Scripts/spawnTrajectory.cs
@@ -6,7 +6,7 @@ using System.Linq;
 
 public class SpawnTrajectory : MonoBehaviour
 {
-    private Vector3 finalPoint;
+    private Vector3 contactPoint;
     private List<Vector3> trajectory = new List<Vector3>();
 
     [SerializeField] 
@@ -18,15 +18,12 @@ public class SpawnTrajectory : MonoBehaviour
         
         trajectory.Add(this.transform.position);
 
-        Vector3 v = finalPoint - this.transform.position;
+        Vector3 v = contactPoint - this.transform.position;
 
         Vector3 offset = v/nbPointsTrajectory;
         
         for(int i = 1; i < nbPointsTrajectory - 1; i++){
-            
-            // float newAngleDir = (Random.Range(-angleMax, angleMax));
-            
-            // Debug.Log(newAngleDir);
+
 
             Vector3 out_ = Vector3.zero;
             if(i == 1)
@@ -40,15 +37,19 @@ public class SpawnTrajectory : MonoBehaviour
             
             trajectory.Add(out_);
 
-            // currentDirection = currentDirection - finalPoint;
 
         }
 
-        trajectory.Add(finalPoint);
+        trajectory.Add(contactPoint);
 
+        for(int i = nbPointsTrajectory -2 ; i >=0  ; i--)
+        {
+            trajectory.Add(-trajectory[i]);
+        }
+
+        trajectory.Add(-this.transform.position);
         this.GetComponent<PlaneBehaviour>().trajectory = this.trajectory;
         this.GetComponent<LineRenderer>().positionCount = trajectory.Count;
-        // Debug.Log("count" + trajectory.Count);
         this.GetComponent<LineRenderer>().SetPositions(trajectory.ToArray());
     }
 }
