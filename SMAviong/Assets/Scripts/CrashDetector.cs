9c75bf0eb961bcf949bd7e87ffbc12822c9c7262
Thibault <thibault3470@outlook.fr>
2023-10-17 09:07:10 +0200

    update

--- SMAviong/Assets/Scripts/CrashDetector.cs
+++ SMAviong/Assets/Scripts/CrashDetector.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] GameObject parent;
+    [SerializeField] GameObject fx;
+
+    private void Start()
+    {
+        fx = GameObject.FindGameObjectWithTag("fx");
+    }
 
     void OnTriggerEnter(Collider other)
     {
         CrashDetector otherCrashdetector = other.GetComponent<CrashDetector>();
         if (otherCrashdetector != null)
         {
+            Instantiate(fx, parent.transform.position, Quaternion.identity);
             Debug.Log("AVION CRASH BOUM BOUMM");
             Destroy(parent);
         }
